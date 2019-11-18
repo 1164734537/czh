23dc00bec3ddc8ac282a50c6c66962834e0bf994
chenzihang <1164734537@qq.com>
2019-11-18 18:02:46 +0800

    更新样式

--- js/index.js
+++ js/index.js
@@ -1,1 +1,36 @@
-console.log('1')
+// console.log($)
+$(function(){
+
+	// 防抖
+	// function debounce(fn,wait){
+	// 	let timeout;
+	// 	return function(){
+	// 		let that = this;
+
+	// 		if(timeout) clearTimeout(timeout);
+
+	// 		let callNow = !timeout;
+	// 		timeout = setTimeout(()=>{
+	// 			timeout = null;
+
+	// 		},wait)
+
+	// 		if(callNow) fn.apply(that)
+	// 	}
+	// }
+
+
+	$('.shop_car').mouseenter(function(){
+		$(this).css('backgroundColor','#fff');
+		$('.shop_car a span').add($('.shop_car a i')).css('color','#ff6700');
+		$('.shop_list').slideDown(400)
+	}).mouseleave(function(){
+		$('.shop_list').slideUp(300,function(){
+			$('.shop_car').css('backgroundColor','#333');
+			$('.shop_car a span').add($('.shop_car a i')).css('color','#b0b0b0');
+		})
+	})	
+})
+
+
+
